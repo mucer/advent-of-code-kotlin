bdd0fb9728fc82e228b64fe2c8c822cda6b77065
Daniel Leib <leib.mobil@gmail.com>
2023-12-07 20:21:14 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
+import java.lang.RuntimeException
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<String>): Int = input
+            .map { "${it.find(Char::isDigit)}${it.findLast(Char::isDigit)}" }
+            .map(String::toInt)
+            .reduce { a, b -> a + b }
+
+    fun getNumber(text: String, last: Boolean): Int {
+        val find =
+                if (last) text.findLastAnyOf(words)
+                else text.findAnyOf(words)
+        if (find == null) throw RuntimeException("No digit in text '$text' found!")
+        return (words.indexOf(find.second) / 2) + 1
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = input
+            .map { "${getNumber(it, false)}${getNumber(it, true)}" }
+            .map(String::toInt)
+            .reduce { a, b -> a + b }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    // test if impl
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+val words = listOf(
+        "1", "one",
+        "2", "two",
+        "3", "three",
+        "4", "four",
+        "5", "five",
+        "6", "six",
+        "7", "seven",
+        "8", "eight",
+        "9", "nine")
